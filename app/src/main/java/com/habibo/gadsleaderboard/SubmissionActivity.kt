801ad1768a0455cb4b7e3b070bc74d54d9d9cf1c
jur1 <yur1m4nuel@gmail.com>
2020-09-12 16:50:59 +0200

    wip

--- app/src/main/java/com/habibo/gadsleaderboard/SubmissionActivity.kt
+++ app/src/main/java/com/habibo/gadsleaderboard/SubmissionActivity.kt
@@ -3,15 +3,17 @@ package com.habibo.gadsleaderboard
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
-import com.habibo.gadsleaderboard.ui.main.SectionsPagerAdapter
 
 class SubmissionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_submission)
+        val goBackBtn = findViewById<Button>(R.id.action_back)
+
+        goBackBtn.setOnClickListener {
+            finish()
+        }
 
 
     }
