5efb08b05b42e3f56f1f9e39931dac0e87499b4f
jur1 <yur1m4nuel@gmail.com>
2020-09-12 17:54:11 +0200

    done

--- app/src/main/java/com/habibo/gadsleaderboard/SubmissionActivity.kt
+++ app/src/main/java/com/habibo/gadsleaderboard/SubmissionActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import cn.pedant.SweetAlert.SweetAlertDialog
+import com.habibo.gadsleaderboard.retrofit.RetrofitInitializer
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SubmissionActivity : AppCompatActivity() {
 
@@ -13,14 +18,59 @@ class SubmissionActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.action_back).setOnClickListener { finish() }
 
-
-        val submitBtn = findViewById<Button>(R.id.action_submit)
         val editTextFirstName = findViewById<EditText>(R.id.editTextFirstName)
         val editTextLastName = findViewById<EditText>(R.id.editTextLastName)
         val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
         val editTextGithubLink = findViewById<EditText>(R.id.editTextGithubLink)
 
+        findViewById<Button>(R.id.action_submit).setOnClickListener {
 
+            SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                .setTitleText(getString(R.string.dialog_sure))
+                .setConfirmText(getString(R.string.dialog_positive))
+                .setCancelText(getString(R.string.dialog_negativo))
+                .setConfirmClickListener {
+                    it.dismiss()
+                    val retrofit = RetrofitInitializer.initializeSubmit()
+                    val call = retrofit.submitProject(
+                        editTextEmail.text.toString(),
+                        editTextFirstName.text.toString(),
+                        editTextLastName.text.toString(),
+                        editTextGithubLink.text.toString()
+                    )
+
+                    call.enqueue(object : Callback<Void> {
+                        override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                            sucessDialog()
+                        }
+
+                        override fun onFailure(call: Call<Void>, t: Throwable) {
+                            errorDialog()
+                        }
+                    })
+
+
+                }.show();
+
+
+        }
+
+
+    }
+
+    private fun errorDialog() {
+        SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+            .setTitleText("Error!")
+            .setContentText("Error While Submitting")
+            .show();
+
+    }
+
+    private fun sucessDialog() {
+        SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+            .setTitleText("Success!")
+            .setContentText("Submission Successful")
+            .show();
     }
 
 }
